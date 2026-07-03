049140395b7c952ae866c652dd388e9674278e0d
JDCloud <admin@jdcloud.local>
2026-07-04 01:05:11 +0800

    项目优化：京东云路由器专用，移除非京东云平台及NIKKI代理
    - 删除IPQ807X/MTK/Rockchip/X86平台配置文件
    - 精简IPQ60XX配置为仅京东云设备（雅典娜RE-CS-02/鲁班RE-SS-01/RE-CS-07/Redmi AX5 JDCloud）
    - 删除MTK-ALL/OWRT-ALL流水线，QCA-ALL精简为IPQ60XX
    - 代理插件NIKKI替换为OpenClash
    - 更新README为京东云专用说明

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ https://drive.wrt.moe/uboot/mediatek
 
 固件信息里的时间为编译开始的时间，方便核对上游源码提交时间。
 
-MEDIATEK系列、QUALCOMMAX系列、ROCKCHIP系列、X86系列。
+京东云路由器专用（QUALCOMMAX IPQ60xx 系列）：雅典娜 RE-CS-02、鲁班 RE-SS-01、RE-CS-07、Redmi AX5 JDCloud 版。
 
 # 目录简要说明
 
--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -56,7 +56,7 @@ UPDATE_PACKAGE "noobwrt" "nooblk-98/luci-theme-noobwrt" "master"
 UPDATE_PACKAGE "shadcn" "eamonxg/luci-theme-shadcn" "main"
 UPDATE_PACKAGE "theme-fluent" "LazuliKao/luci-theme-fluent" "main"
 
-UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
+UPDATE_PACKAGE "luci-app-openclash" "vernesong/OpenClash" "master"
 
 #UPDATE_PACKAGE "athena-led" "unraveloop/JDC-AX6600-Athena-LED-Controller" "main"
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
